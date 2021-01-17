414e9783551872dfd0dd0ce6193ded589469fedf
seireitou <ausis@mail.com>
2021-01-16 18:18:29 -0600

    commit tree aa7c9febdc653c25436a87f850637d134e9f355a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+require 'pry'
 # Add  code here!
+def prime?(n)
+    if n > 1
+        range = (2..n-1).to_a
+        prime = range.none? do |num|
+            n % num == 0
+        end
+        
+        prime
+    else
+        return false
+    end
+end
